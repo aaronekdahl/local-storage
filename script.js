df9c613840613f403ff2f9877d0a6577e942bbc4
aaronekdahl <aaronekahl@me.com>
2017-05-25 10:28:59 -0700

    lunchtime-hw-session

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-$(function() {
-
 var planets = [
 	{name: 'mercury', distance: 36},
 	{name: 'venus', distance: 67.2},
@@ -10,7 +8,6 @@ var planets = [
 	{name: 'uranus', distance: 1784.0},
 	{name: 'neptune', distance: 2794.4}
 	];
-});
 
 /*This handler is for the new-item button. It should hide the table and show the form. 
 It should set a handler for the form's submit button.*/
@@ -26,7 +23,7 @@ $(function() {
 /* This function creates a table row (tr) for each object in the array, with a cell (td) 
 for each property to be displayed. Append each of these rows to the table body.*/
 
-$(function() {
+function displayPlanets() {
 
 var tableBody = $('#makeTable');
 for (var i = 0; i < planets.length; i++) {
@@ -36,24 +33,30 @@ for (var i = 0; i < planets.length; i++) {
 	row.append($('<td></td>').text(planets[i].distance));
 	tableBody.append($row);
 }
-
-});
+};
 
 
 /* This handler is for the submit button that creates an object using the form data and 
 adds that object to the array. Then it should call the first function you wrote to 
 recreate the table with the updated database and show that table, hiding the form.*/
 
-$(function () {
-
-    $('#add').on('click', function () {
-        event.preventDefault();
-        var $table = $('#dataTable');
-        var $tr = $('#sec01').find('$row').first();
-        //$table.find("tbody").append($tr);
-        $table.row.append($tr);
-    });
+$('#add').on('click', function() {
+    event.preventDefault();
+    var planetName = $('#name').val();
+    var planetDistance = $('#distance').val();
 });
+
+
+// $(function() {
+
+//     $('#add').on('click', function () {
+//         event.preventDefault();
+//         var $table = $('#dataTable');
+//         var $tr = $('#sec01').find('$row').first();
+//         //$table.find("tbody").append($tr);
+//         $table.row.append($tr);
+//     });
+// });
 
 
 
